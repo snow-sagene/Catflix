4736231e92beea18dbfaebe9d666b41d079472a2
FLEKKE\vtr04 <0429lyb@naver.com>
2024-02-03 19:30:28 +0900

    깃허브와 첫 연결

---
+++ register.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Catflix</title>
+    <link rel = "stylesheet" type = " text/css" href="assets/style/style.css"/>
+</head>
+<body>
+    <div class="signInContainer">
+        <div class="column">
+            <form method= "POST"> 
+                <input type = "text">
+            </form>
+        </div> 
+
+    </div>
+
+</body>
+</html>
